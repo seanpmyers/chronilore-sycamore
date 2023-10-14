52c8bcb0f78a438b9b91e56c5d7e5f73723c9e59
Sean Myers <spmyers@protonmail.com>
2023-10-14 09:44:17 -0400

    updating project structure

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -4,6 +4,5 @@ use perseus::prelude::*;
 
 #[perseus::main(perseus_axum::dflt_server)]
 pub fn main<G: Html>() -> PerseusApp<G> {
-    PerseusApp::new()
-        .template(crate::templates::index::get_template())
+    PerseusApp::new().template(crate::templates::index::get_template())
 }
