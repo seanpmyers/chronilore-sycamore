83854423e54e7d3d56987fc0edef6161049a3175
Sean Myers <spmyers@protonmail.com>
2023-12-10 11:20:55 -0500

    adding working stopwatch

--- sycamore9/src/components/timer.rs
+++ sycamore9/src/components/timer.rs
@@ -1,27 +1,76 @@
 use futures_util::{future::ready, stream::StreamExt};
 use gloo_timers::future::IntervalStream;
-use sycamore::prelude::*;
-use time::Instant;
+use js_sys::Date;
+use sycamore::{futures::spawn_local_scoped, prelude::*};
+
+const SECONDS_IN_MILLISECOND: f64 = 1000_f64;
+const TIMER_UPDATE_FREQUENCY_MILLISECONDS: u32 = 2;
+const ZERO_SECONDS: f64 = 0_f64;
 
 #[derive(Props)]
 pub struct TimerProperties {
-    pub stopwatch: &'static str,
+    pub reset: Signal<bool>,
+    pub paused: Signal<bool>,
+    pub elapsed: Signal<f64>,
 }
 
 #[component]
-pub async fn Timer<G: Html>(TimerProperties { stopwatch }: TimerProperties) -> View<G> {
-    let stopwatch: Signal<&'static str> = create_signal(stopwatch);
+pub async fn Timer<G: Html>(
+    TimerProperties {
+        reset,
+        paused,
+        elapsed,
+    }: TimerProperties,
+) -> View<G> {
+    let browser_time: Signal<f64> = create_signal(Date::new_0().get_time());
+    let stopwatch: ReadSignal<String> = create_memo(move || format!("{:.2}", elapsed.get()));
+    let time_at_pause: Signal<f64> = create_signal(ZERO_SECONDS);
     if G::IS_BROWSER {
-        let rust_time = Instant::now();
-        IntervalStream::new(0)
-            .for_each(|_| {
-                // stopwatch.set();
-                ready(())
-            })
-            .await;
+        create_effect(move || {
+            paused.track();
+            if paused.get() && !reset.get() {
+                time_at_pause.set(
+                    time_at_pause.get()
+                        + ((Date::new_0().get_time() - browser_time.get())
+                            / SECONDS_IN_MILLISECOND),
+                );
+            } else {
+                browser_time.set(Date::new_0().get_time());
+            }
+        });
+        create_effect(move || {
+            reset.track();
+            reset.get();
+            paused.set(true);
+            elapsed.set(ZERO_SECONDS);
+            time_at_pause.set(ZERO_SECONDS);
+            reset.set(false);
+        });
+        stopwatch.track();
+        reset.track();
+        paused.track();
+        browser_time.track();
+        time_at_pause.track();
+        elapsed.track();
+        spawn_local_scoped(async move {
+            IntervalStream::new(TIMER_UPDATE_FREQUENCY_MILLISECONDS)
+                .for_each(|_| {
+                    if paused.get() {
+                        return ready(());
+                    }
+                    elapsed.set(
+                        time_at_pause.get()
+                            + ((Date::new_0().get_time() - browser_time.get())
+                                / SECONDS_IN_MILLISECOND),
+                    );
+                    ready(())
+                })
+                .await;
+        });
     }
 
     view! {
+        div() { "Timer" }
         div() { (stopwatch.get_clone()) }
     }
 }
--- sycamore9/src/main.rs
+++ sycamore9/src/main.rs
@@ -1,8 +1,10 @@
-use sycamore::prelude::*;
+use sycamore::{prelude::*, suspense::Suspense};
+use web_sys::MouseEvent;
 
 use crate::components::{
     icon::{MOON_SVG_HTML, SUN_SVG_HTML},
     theme_toggle::ThemeToggle,
+    timer::Timer,
 };
 
 pub mod components;
@@ -12,6 +14,11 @@ fn App<G: Html>() -> View<G> {
     let mut state: Signal<i32> = create_signal(0);
 
     let increment = move |_| state += 1;
+    let paused: Signal<bool> = create_signal(true);
+    let reset: Signal<bool> = create_signal(true);
+    let elapsed: Signal<f64> = create_signal(0_f64);
+    let toggle_pause = move |_: MouseEvent| paused.set(!paused.get());
+    let toggle_reset = move |_: MouseEvent| reset.set(true);
 
     view! {
         div {
@@ -20,6 +27,11 @@ fn App<G: Html>() -> View<G> {
             button(on:click=increment) { "+" }
             span() {(state.get())}
             ThemeToggle(text="", light_icon=SUN_SVG_HTML, dark_icon=MOON_SVG_HTML)
+            button(on:click=toggle_pause) { "Start/Stop" }
+            button(on:click=toggle_reset) { "Reset" }
+            Suspense(fallback=view!{ "Loading..." }) {
+                Timer(reset=reset, paused=paused, elapsed=elapsed)
+            }
         }
     }
 }
